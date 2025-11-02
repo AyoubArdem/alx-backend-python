ed50c6b77f9a0cbd9343e7671783b36033d271b4
AYOUB <ayoub.ardem@gmail.com>
2025-11-02 15:59:32 +0100

    Update batch_processing.py

--- python-generators-0x00/batch_processing.py
+++ python-generators-0x00/batch_processing.py
@@ -25,6 +25,27 @@ def stream_users_in_batches(batch_size):
     connection.close()
 
 
+
+def batch_processing(batch_size):
+     for batch in stream_users_in_batches(batch_size):
+         for user in batch:
+             if user[age] > 25:
+                 print(user)
+     
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
 #methode_2
 import mysql.connector
 
@@ -50,3 +71,5 @@ def stream_users_in_batches(batch_size):
 
     cursor.close()
     connection.close()
+
+
