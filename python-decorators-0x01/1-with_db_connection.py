c344cdac1e3989883834e1421c3d1e67d671dd9b
AYOUB <ayoub.ardem@gmail.com>
2025-11-06 16:02:49 +0100

    Update 1-with_db_connection.py

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -2,18 +2,21 @@ import sqlite3
 import functools
 
 def with_db_connection(func):
-  @wrap(func)
-  def wrapper(*args,**kwargs):
-    conn=sqlite3.connect('users.db')
-    func(*args,**kwargs)
-    return conn
-  return wrapper
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper
 
 @with_db_connection
-def get_user_by_id(conn,user_id):
-  cursor=conn.cursor()
-  cursor.execute("SELECT * FROM users WHERE id=?",(user_id,))
-  return cursor.fetchone()
-  
-user=get_user_by_id(user_id=1)
+def get_user_by_id(conn, user_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
+    return cursor.fetchone()
+
+user = get_user_by_id(user_id=1)
 print(user)
