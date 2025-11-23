b0a9cbcada47e2b5956a1f7e99541c0c303db40b
ayoub-student <https://github.com/ayoub-student>
2025-11-23 18:12:08 +0100

    new update

--- Django-Middleware-0x03/chats/auth.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simpletoken.tokens import RefreshToken
-from django.contrib.auth.models import User
-
-class get_tokens_for_users(User):
-     refresh_token=RefreshToken.for_user(User)
-     return {
-       "refresh":str(refresh_token),
-       "access":str(refresh_token.access_token),
-     }
