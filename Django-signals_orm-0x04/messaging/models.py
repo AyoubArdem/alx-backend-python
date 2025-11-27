f3836ef04c8b9ee152f0a93b7b266d3646845bc0
AYOUB <ayoub.ardem@gmail.com>
2025-11-27 14:52:51 +0100

    Create models.py

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,26 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sender} → {self.receiver}: {self.content[:20]}"
+
+
+  
+class Notifications(models.Model):
+    user = models.ForeingKey(User , on_delete=models.Cascade)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    seen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Notification for {self.user}"
