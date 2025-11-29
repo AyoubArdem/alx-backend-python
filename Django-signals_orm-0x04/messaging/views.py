0853b48c919da55331c191a0d0b0cc0f834a85b4
AYOUB <ayoub.ardem@gmail.com>
2025-11-29 14:28:20 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,14 @@
 
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from .models import Message
 
 def delete_user(request, user_id):
     user = User.objects.get(id=user_id)
     user.delete()
     return JsonResponse({"status": "User deleted"})
+
+def UnreadMessagesManager(request,user_id):
+    messages = Message.unread.for_user(user_id)
+    data = [{"id":msg.id,"content":msg.content,"sender":msg.sender.username} for msg in messages]
+    return JsonResponse({"unread messages":data})
