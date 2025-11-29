b3cb796b1d7df9f0ee1324468c9e4f7373ca2b5b
AYOUB <ayoub.ardem@gmail.com>
2025-11-29 14:52:36 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,29 @@
-
 from django.contrib.auth.models import User
-from django.http import JsonResponse
+from django.db.models import Prefetch
 from .models import Message
+from .utils import get_replies
+from django.http import JsonResponse
 
+
+def conversation_thread(request, user_id):
+    messages = (
+        Message.objects.filter(receiver_id=user_id, parent_message__isnull=True)
+        .select_related("sender", "receiver")
+        .prefetch_related(
+            Prefetch("message_set", queryset=Message.objects.select_related("sender"))
+        )
+    )
+
+    data = []
+    for msg in messages:
+        data.append({
+            "id": msg.id,
+            "content": msg.content,
+            "sender": msg.sender.username,
+            "replies": get_replies(msg)
+        })
+    return JsonResponse({"threads:",data}}
+                        
 def delete_user(request, user_id):
     user = User.objects.get(id=user_id)
     user.delete()
